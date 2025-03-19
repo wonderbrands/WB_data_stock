6b0646e8aecd33e2c5edaf075442e8a4e51874c7
demianavilar@gmail.com <Demian Avila>
2025-03-19 20:03:38 +0100

    fix: modified to install in v18

--- __manifest__.py
+++ __manifest__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Inventory',
-    'version': '15.0',
+    'version': '18.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
@@ -30,11 +30,6 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/stock_picking_views.xml',
-        'views/views.xml',
-        'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
 }
